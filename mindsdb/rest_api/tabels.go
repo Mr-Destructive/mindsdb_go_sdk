7dbd0b848faadfbb42709a09c6c912eb3b06047a
mr_destructive <gormeet711@gmail.com>
2023-05-30 20:11:38 +0530

    chore: table create and delete

--- mindsdb/rest_api/tabels.go
+++ mindsdb/rest_api/tabels.go
@@ -20,3 +20,17 @@ func GetTable(api *connectors.RestAPI, database, name string) ([]byte, error) {
 	mindsdb.HandleError(err)
 	return resp, err
 }
+
+func CreateTable(api *connectors.RestAPI, database string, body map[string]string) ([]byte, error) {
+	endpoint := fmt.Sprintf("/api/databases/%s/tables", database)
+	resp, err := api.APIRequest("", "POST", endpoint, body)
+	mindsdb.HandleError(err)
+	return resp, err
+}
+
+func DeleteTable(api *connectors.RestAPI, database, name string) ([]byte, error) {
+	endpoint := fmt.Sprintf("/api/databases/%s/tables/%s", database, name)
+	resp, err := api.APIRequest("", "DELETE", endpoint, map[string]string{})
+	mindsdb.HandleError(err)
+	return resp, err
+}
